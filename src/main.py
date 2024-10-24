138552b07e76939846000751ac01de4bd43d9d3e
halvor <halvorbmundal@gmail.com>
2024-10-24 11:04:35 +0200

    Legg til publisher og description på katalog

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from uuid import uuid4
 
 from datacatalogtordf import Catalog
 
+from src.constants import ENTUR_DATANORGE_PAGE
 from src.resources.national_stop_registry import get_national_stop_registry_resourses
 from src.resources.geocoder import create_geocoder_data_service
 from src.resources.journey_planner import create_journey_planner_service
@@ -14,8 +15,10 @@ from src.resources.timetable import create_time_table_resources
 def main():
     # Create catalog object
     enturCatalog = Catalog()
-    enturCatalog.identifier = "https://entur.no/" 
+    enturCatalog.identifier = "https://entur.org/"
     enturCatalog.title = {"en": "Entur Data Catalog"}
+    enturCatalog.publisher = ENTUR_DATANORGE_PAGE
+    enturCatalog.description = {"en": "Entur data catalogue"}
 
     # Add datasets and services to the catalog
     national_stop_registry_dataset, national_stop_registry_data_service = get_national_stop_registry_resourses()
@@ -27,17 +30,17 @@ def main():
     mobility_data_service = create_mobility_service()
 
     enturCatalog.datasets.extend([
-        national_stop_registry_dataset,
-        timetable_dataset,
+        #national_stop_registry_dataset,
+        #timetable_dataset,
     ])
     enturCatalog.services.extend([
-        national_stop_registry_data_service,
-        geocoder_data_service,
-        journey_planner_service,
-        timetable_data_service,
-        realtime_data_service,
+        #national_stop_registry_data_service,
+        #geocoder_data_service,
+        #journey_planner_service,
+        #timetable_data_service,
+        #realtime_data_service,
         station_distances_data_service,
-        mobility_data_service
+        #mobility_data_service
     ])
 
     # Get RDF representation in Turtle format
