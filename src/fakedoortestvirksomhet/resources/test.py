1772393ad6fa5fb57abeed99c21b057fdc3b6314
halvor <halvorbmundal@gmail.com>
2024-12-13 13:12:25 +0100

    Oppdater navn på fakedoor datasett

--- src/fakedoortestvirksomhet/resources/test.py
+++ src/fakedoortestvirksomhet/resources/test.py
@@ -8,7 +8,7 @@ def create_test_dataset():
     dataset = Dataset()
     dataset.identifier = "https://test-data/identifikator-for-datasettet"
     dataset.title = {
-        "no": "Fakedoor test"
+        "no": "Test av fakedoor"
     }
     dataset.publisher = "https://staging.fellesdatakatalog.digdir.no/organizations/210841962"
     dataset.description = {
